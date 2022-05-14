4b8ab1614c72ff1ca49f9897e2e369933925d8e5
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-05-14 14:37:29 +0000

    [Rspec]test

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -7,5 +7,36 @@ describe "投稿のテスト" do
   it "be_valid" do
     expect(list).to be_valid
   end
-  describe
+  describe "トップ画面のテスト" do
+    before "トップ画面への遷移" do
+      visit root_path
+    end
+    context "表示の確認" do
+      it "トップ画面(top_path)に「ここはTopページです」が表示されているか" do
+        expect(page).to have_content "ここはTopページです"
+      end
+      it "top_pathが\"/top\"であるか" do
+        expect(page).to have_link "Top" ,href: "/top"
+      end
+    end
+  end
+  
+  describe "投稿画面のテスト" do
+    before "投稿画面への遷移" do
+      visit new_list_path
+    end
+    context "表示の確認 " do
+      it "new_list_pathが\"/lists/new\"であるか" do
+        expect(page).to have_current_path new_list_path
+      end
+      it "投稿ボタンが表示されているか" do
+        expect(page).to have_button "New list"
+      end
+    end
+    context "投稿処理のテスト" do
+      it "投稿後のリダイレクト先は正しいか" do
+        
+      end
+    end
+  end
 end

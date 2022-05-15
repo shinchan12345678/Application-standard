894375c3ffbbcc99a4a9b230d3a794d1a4280d15
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-05-15 00:59:42 +0000

    [途中経過の保存]

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -20,7 +20,7 @@ describe "投稿のテスト" do
       end
     end
   end
-  
+
   describe "投稿画面のテスト" do
     before "投稿画面への遷移" do
       visit new_list_path
@@ -35,8 +35,52 @@ describe "投稿のテスト" do
     end
     context "投稿処理のテスト" do
       it "投稿後のリダイレクト先は正しいか" do
-        
+        fill_in "book[title]", with: Faker::Lorem.characters(number:5)
+        fill_in "book[body]" , with: Faker::Lorme.characters(number:20)
+        click_buttun "投稿"
+        expect(page).to have_current_path list_path(List.last)
       end
     end
   end
+
+  describe "一覧画面のテスト" do
+    before "一覧画面への遷移" do
+      visit lists_path
+    end
+    context "一覧の表示とリンクの確認" do
+      it "一覧表示画面に投稿されたもの表示されているか" do
+        expext(page).to have_content list.title
+        expext(page).to have_content list.body
+      end
+    end
+  end
+
+  describe "詳細画面のテスト" do
+    before "詳細画面への遷移" do
+      visit list_path(list)
+    end
+    context "表示のテスト" do
+      it "削除リンクが存在しているか" do
+        expect(page).to have_link "削除"
+      end
+      it "編集リンクが存在しているか" do
+        expect(page).to have_link "編集"
+      end
+    end
+    context "リンクの遷移先の確認" do
+      it "編集の遷移先は編集画面か" do
+        click_buttun "編集"
+        expect(page).to have_current_path
+      end
+    end
+    context "list削除のテスト" do
+      it "listの削除" do
+        edit_link = find_all("a")[3]
+        edit_link.click
+        expect(current_path).to eq('/list/'+list.id.to_s+'/edit')
+      end
+    end
+  end
+
+
 end
